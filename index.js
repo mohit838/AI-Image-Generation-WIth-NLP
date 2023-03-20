830737ccc7f58d9c9933260260db80a74dc59d02
Mohammad Mohitul Islam <mohitul449@gmail.com>
2023-03-20 17:33:03 +0600

    Get Image Using OpenAI

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+// Enable Body Parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/openai", require("./routes/openAIRoutes"));
 
 // Port Testing
--- routes/openAIRoutes.js
+++ routes/openAIRoutes.js
@@ -1,10 +1,7 @@
 const express = require("express");
+const { generateAIImage } = require("../controllers/OpenAIController");
 const router = express.Router();
 
-router.post("/generate-ai-image", (req, res) => {
-  res.status(200).json({
-    success: true,
-  });
-});
+router.post("/generate-ai-image", generateAIImage);
 
 module.exports = router;
